bc84580aa2476f33e7ed4a7ae82a9038dbb07644
thebadger135 <badger@small-affair.com>
2012-11-09 17:40:15 -0800

    Working on CompletedTasks

---
+++ ToDo Cloud/ToDoCloudCompletedTasks.h
@@ -0,0 +1,13 @@
+//
+//  ToDoCloudCompletedTasks.h
+//  ToDo Cloud
+//
+//  Created by Jordan Bargas on 11/9/12.
+//  Copyright (c) 2012 Alexander Noguchi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoCloudCompletedTasks : NSObject
+
+@end
---
+++ ToDo Cloud/ToDoCloudCompletedTasks.m
@@ -0,0 +1,13 @@
+//
+//  ToDoCloudCompletedTasks.m
+//  ToDo Cloud
+//
+//  Created by Jordan Bargas on 11/9/12.
+//  Copyright (c) 2012 Alexander Noguchi. All rights reserved.
+//
+
+#import "ToDoCloudCompletedTasks.h"
+
+@implementation ToDoCloudCompletedTasks
+
+@end
